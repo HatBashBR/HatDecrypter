1b938b9fbe4ab9c12afbfde6dca416cc56ca4a4a
Everton Chapani <ecchapani1@hotmail.com>
2017-03-10 23:13:17 -0300

    Corrigidos alguns bugs e adicionada nova funcionalidade (-p)

--- README.md
+++ README.md
@@ -1,11 +1,20 @@
 # HatDecrypt
 MD5 and SHA1 Decryptor
 
-#Dependencies
+# Dependencies
 You will need to install the bs4 library:
 pip install bs4
 or download at https://pypi.python.org/pypi/beautifulsoup4
 
-#Usage
-python hatdecrypt.py -t 0 //MD5 Decrypt<br />
-python hatdecrypt.py -t 1 //SHA1 Decrypt
+# Types List
+0 = MD5<br />
+1 = SHA1
+
+# Options
+Options:<br />
+  -h, --help            show this help message and exit<br />
+  -t TIPO, --type=TIPO  TIPO(deve estar na lista)<br />
+  -p HASH, --pass=HASH  adicione o hash(opcional)
+
+# Usage
+python hatdecrypt.py -t TIPO
--- hatdecrypter.py
+++ hatdecrypter.py
@@ -5,43 +5,50 @@ import optparse
 #Author: Everton a.k.a XGU4RD14N && Mateus Lino a.k.a Dctor
 #fb: https://www.facebook.com/hatbashbr/
 
-def md5Decrypt(hash=""):
+def md5Decrypt(hash):
     print "MD5 Decrypter"
-    if(hash == ""):
+    if(hash == "None"):
         hash = raw_input("Digite o hash: ")
     url = BeautifulSoup(urllib.urlopen("https://md5.gromweb.com/?md5=" + hash), "html.parser")
     password = url.find("em", {"class": "long-content string"})
     password = re.sub(re.compile("<.*?>"), "", str(password)).strip()
-    print "Senha: " + password
+    if str(password) == "None":
+        print "Senha nao encontrada! :-("
+    else:
+        print "Senha: " + password
 
-def sha1Decrypt(hash=""):
+def sha1Decrypt(hash):
     print"SHA1 Decrypter"
-    if(hash == ""):
+    if(hash == "None"):
         hash = raw_input("Digite o hash: ")
     url = BeautifulSoup(urllib.urlopen("https://sha1.gromweb.com/?hash=" + hash), "html.parser")
     password = url.find("em", {"class": "long-content string"})
     password = re.sub(re.compile("<.*?>"), "", str(password)).strip()
-    print "Senha: " + password
+    if str(password) == "None":
+        print "Senha nao encontrada! :-("
+    else:
+        print "Senha: " + password
 
 parser = optparse.OptionParser()
-parser.add_option("-t", "--type", dest="type", help="0 for MD5, 1 for SHA1")
-v = optparse.Values()
-(options, args) = parser.parse_args(values=v)
+parser.add_option("-t", "--type", dest="tipo", help="TIPO(deve estar na lista)", default="None")
+parser.add_option("-p", "--pass", dest="hash", help="adicione o hash(opcional)", default="None")
+options, args = parser.parse_args()
 
 try:
-    type = int(options.type)
-    if len(v.__dict__) == 0:
+    if options.tipo == "None":
         print "Voce precisa definir um tipo de hash!"
         parser.print_help()
         sys.exit()
+
+    type = int(options.tipo)
     if(type != 0 and type != 1):
         print "Tipo de hash invalido!"
         sys.exit()
 
     if(type == 0):
-        md5Decrypt()
+        md5Decrypt(options.hash)
     elif(type == 1):
-        sha1Decrypt()
+        sha1Decrypt(options.hash)
     else:
         parser.print_help()
         sys.exit()
