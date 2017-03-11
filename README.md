ff91e6e70a4b6ecfbd161ec92519111d85d74389
Everton Chapani <ecchapani1@hotmail.com>
2017-03-11 17:32:58 -0300

    Adicionado suporte a quebra de hashes com salt

--- README.md
+++ README.md
@@ -14,7 +14,13 @@ or download at https://pypi.python.org/pypi/beautifulsoup4
 Options:<br />
   -h, --help            show this help message and exit<br />
   -t TIPO, --type=TIPO  TIPO(deve estar na lista)<br />
-  -p HASH, --pass=HASH  adicione o hash(opcional)
+  -p HASH, --pass=HASH  adicione o hash(opcional)<br />
+  -w WL, --wordlist=WL  adicione uma wordlist(opcional)<br />
+  -s SALT, --salt=SALT  adicione um salt(opcional)
 
 # Usage
-python hatdecrypt.py -t TIPO
+Default<br />
+python hatdecrypter.py -t TIPO<br /><br />
+
+Salted<br />
+python hatdecrypter.py -t TIPO -s SALT
--- hatdecrypter.py
+++ hatdecrypter.py
@@ -1,12 +1,34 @@
-import urllib, re, sys
+import urllib, re, sys, optparse, hashlib
 from bs4 import BeautifulSoup
-import optparse
 
 #Author: Everton a.k.a XGU4RD14N && Mateus Lino a.k.a Dctor
 #fb: https://www.facebook.com/hatbashbr/
 
+def md5salt(hash):
+    print "########## Tentando - MD5 Salted Decrypter ##########"
+    if(hash == "None"):
+        hash = raw_input("Digite o hash: ")
+        
+    if(options.salt == "None"):
+        print "Para tentar com o MD5 Salted voce precisa definir um salt!"
+        sys.exit()
+    
+    try:
+        f = open(options.wl)
+        for pwd in f.readlines():
+            pwd = pwd.strip()
+            d = hashlib.md5(options.salt+pwd).hexdigest()
+            if(d == hash):
+                print "Senha encontrada: "+pwd
+                sys.exit()
+        print "Senha nao encontrada! :-("
+    except IOError:
+        print "Nao foi possivel abrir sua wordlist, tente novamente."
+    except Exception as e:
+        print "Erro: "+str(e)
+    
 def md5Decrypt(hash):
-    print "MD5 Decrypter"
+    print "########## Tentando - MD5 Decrypter ##########"
     if(hash == "None"):
         hash = raw_input("Digite o hash: ")
     url = BeautifulSoup(urllib.urlopen("https://md5.gromweb.com/?md5=" + hash), "html.parser")
@@ -17,8 +39,31 @@ def md5Decrypt(hash):
     else:
         print "Senha: " + password
 
+def sha1salt(hash):
+    print "########## Tentando - SHA1 Salted Decrypter ##########"
+    if(hash == "None"):
+        hash = raw_input("Digite o hash: ")
+        
+    if(options.salt == "None"):
+        print "Para tentar com o SHA1 Salted voce precisa definir um salt!"
+        sys.exit()
+    
+    try:
+        f = open(options.wl)
+        for pwd in f.readlines():
+            pwd = pwd.strip()
+            d = hashlib.sha1(options.salt+pwd).hexdigest()
+            if(d == hash):
+                print "Senha encontrada: "+pwd
+                sys.exit()
+        print "Senha nao encontrada! :-("
+    except IOError:
+        print "Nao foi possivel abrir sua wordlist, tente novamente."
+    except Exception as e:
+        print "Erro: "+str(e)
+
 def sha1Decrypt(hash):
-    print"SHA1 Decrypter"
+    print"########## Tentando - SHA1 Decrypter ##########"
     if(hash == "None"):
         hash = raw_input("Digite o hash: ")
     url = BeautifulSoup(urllib.urlopen("https://sha1.gromweb.com/?hash=" + hash), "html.parser")
@@ -32,6 +77,8 @@ def sha1Decrypt(hash):
 parser = optparse.OptionParser()
 parser.add_option("-t", "--type", dest="tipo", help="TIPO(deve estar na lista)", default="None")
 parser.add_option("-p", "--pass", dest="hash", help="adicione o hash(opcional)", default="None")
+parser.add_option("-w", "--wordlist", dest="wl", help="adicione uma wordlist(opcional)", default="john.txt")
+parser.add_option("-s", "--salt", dest="salt", help="adicione um salt(opcional)", default="None")
 options, args = parser.parse_args()
 
 try:
@@ -45,10 +92,14 @@ try:
         print "Tipo de hash invalido!"
         sys.exit()
 
-    if(type == 0):
+    if(type == 0 and options.salt == "None"):
         md5Decrypt(options.hash)
-    elif(type == 1):
+    elif(type == 0 and options.salt != "None"):
+        md5salt(options.hash)
+    elif(type == 1 and options.salt == "None"):
         sha1Decrypt(options.hash)
+    elif(type == 1 and options.salt != "None"):
+        sha1salt(options.hash)
     else:
         parser.print_help()
         sys.exit()
